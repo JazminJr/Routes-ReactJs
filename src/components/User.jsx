729acfad71bc0665797a78c6ac7dc50c7b500f40
JazminJr <48848731+JazminJr@users.noreply.github.com>
2020-12-18 15:27:09 -0600

    JazminJr

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -2,24 +2,23 @@ import React from 'react'
 import { useParams } from 'react-router-dom'
 
 const User = () => {
-
-    
-    // console.log(useParams());
-    const {id} = useParams()
+    //console.log(useParams)
+    const {id}=useParams()
     console.log(id)
 
     const [pueblo, setPueblo] = React.useState([])
 
-    React.useEffect(() => {
-        const obtenerDatos = async () => {
-            const data = await fetch(`https://age-of-empires-2-api.herokuapp.com/api/v1/civilization/${id}`)
+    React.useEffect(()=>{
+        const obtenerDatos = async()=>{
+            const data = await fetch (`https://age-of-empires-2-api.herokuapp.com/api/v1/civilization/${id}`)
             const users = await data.json()
             setPueblo(users)
         }
+        //console.log('useeffect')
         obtenerDatos()
-    }, [id])
-
-    
+        /*setEquipo(datos)*/
+    },[id])
+   
 
     return (
         <div>
@@ -30,3 +29,4 @@ const User = () => {
 }
 
 export default User
+
